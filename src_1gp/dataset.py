59b65340433972f9aa11a7b86a224fb51e06cc9b
leo <yvquan.li@gmail.com>
2022-02-28 11:59:17 +0800

    Update dataset.py

--- src_1gp/dataset.py
+++ src_1gp/dataset.py
@@ -4,14 +4,17 @@ from random import shuffle
 from pathlib import Path
 from tqdm import tqdm
 import numpy as np
+
 import pandas as pd
 import torch
 from torch_scatter import scatter
 from torch_geometric.data import Data, InMemoryDataset, Batch
-from utils import one_of_k_encoding, random_scaffold_split, scaffold_split_fp
+from utils import one_of_k_encoding, random_scaffold_split, seed_torch
+from torch.nn.functional import one_hot
 
 try:
     import rdkit
+    import sqlite3
     from rdkit import Chem, RDConfig
     from rdkit.Chem import ChemicalFeatures, MolFromSmiles
     from rdkit.Chem.rdchem import HybridizationType as HT
@@ -20,7 +23,7 @@ try:
 
     RDLogger.DisableLog('rdApp.*')
 except:
-    rdkit, Chem, RDConfig, MolFromSmiles, ChemicalFeatures, HT, BT = 7 * [None]
+    rdkit, sqlite3, Chem, RDConfig, MolFromSmiles, ChemicalFeatures, HT, BT = 8 * [None]
     print('Please install rdkit for data processing')
 
 
@@ -65,206 +68,113 @@ def get_mol_nodes_edges(mol):
 
 
 class Dataset(InMemoryDataset):
-    def __init__(self, root, dataset='bbbp', split='scaffold', split_seed=1234, transform=None, pre_transform=None,
+    def __init__(self, root, dataset='drugbank_caster', split='random', split_seed=1234, transform=None,
+                 pre_transform=None,
                  pre_filter=None):
-        self.dataset = dataset  # random / random_nan / scaffold
-        self.split_seed = split_seed
+        self.train_val_test_GLAMs = {'train': 0, 'val': 0, 'test': 0}
+        self.dataset = dataset
         super(Dataset, self).__init__(root, transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
-        trn, val, test = self.split(type=split)
+        self.mol_data = torch.load(self.processed_paths[1])
+        trn, val, test = self.split(type=split, seed=split_seed)
         self.train, self.val, self.test = trn, val, test
-        self.mol_num_node_features = self[0].x.shape[1]
-        self.mol_num_edge_features = self[0].edge_attr.shape[1]
-        self.num_tasks = len(self.tasks)
+        mol_GLAM = list(self.mol_data.values())[0]
+        self.mol_num_node_features = mol_GLAM.x.shape[1]
+        self.mol_num_edge_features = mol_GLAM.edge_attr.shape[1]
 
     @property
     def raw_file_names(self):
-        return ['{}.csv'.format(self.dataset)]
+        if self.dataset == 'drugbank_caster':
+            return ['drugbank_caster/ddi_total.csv']
+        else:
+            raise ValueError('Error dataset input!')
 
     @property
     def processed_file_names(self):
-        return ['dataset_{}.pt'.format(self.dataset)]
+        return ['{}_smi_labels.pt'.format(self.dataset), '{}_mol_data.pt'.format(self.dataset)]
+
+    def ddi2datalist(self, drug1_list: list, drug2_list: list, interaction_list: list, list_all_smiles=None):
+        data_list = []
+        for drug1, drug2, label in tqdm(zip(drug1_list, drug2_list, interaction_list)):
+            drug1 = self.canonical(drug1)
+            drug2 = self.canonical(drug2)
+            if drug1 is None or drug2 is None: continue
+            if list_all_smiles is not None:
+                if drug1 not in list_all_smiles or drug2 not in list_all_smiles: continue
+            data = Data(smi1=drug1, smi2=drug2, y=label)
+            data_list.append(data)
+        return data_list
+
+    def smiles2datadict(self, smiles_list: list):
+        data_dict = {}
+        for smi in tqdm(set(smiles_list)):
+            smi = self.canonical(smi)
+            if smi is None: continue
+            x, edge_index, edge_attr = get_mol_nodes_edges(MolFromSmiles(smi))
+            data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+            data_dict[smi] = data
+        return data_dict
+
+    @staticmethod
+    def canonical(smi):
+        try:
+            return Chem.MolToSmiles(Chem.MolFromSmiles(smi), canonical=True)
+        except:
+            print("failed smiles: ", smi, end='\t')
+            return None
 
     def process(self):
-        # load csv
-        df = pd.read_csv(self.raw_paths[0])
-        target = df[self.tasks].values
-        smiles_list = df.smiles.values
-        data_list = []
+        if self.dataset == 'drugbank_caster':
+            drugbank_total = pd.read_csv(self.raw_paths[0])
+            data_list = self.ddi2datalist(drugbank_total['Drug1_SMILES'].to_list(),
+                                          drugbank_total['Drug2_SMILES'].to_list(),
+                                          drugbank_total['label'].to_list())
+            data, slices = self.collate(data_list)
+            torch.save((data, slices), self.processed_paths[0])
 
-        for i, smi in enumerate(tqdm(smiles_list)):
-            if not self.is_valid_smiles(smi): continue
-            mol = MolFromSmiles(smi)
-            if mol is None: return None
-            x, edge_index, edge_attr = get_mol_nodes_edges(mol)
-            label = target[i]
-            # label[np.isnan(label)] = 6
-            if self.dataset in ['esol', 'freesolv', 'lipophilicity']:
-                y = torch.FloatTensor(label).unsqueeze(0)
-            elif self.dataset in ['demo', 'bbbp', 'bace', 'sider', 'toxcast', 'toxcast', 'tox21']:
-                label[np.isnan(label)] = -1  # Fill in -1 for those NaN labels
-                y = torch.LongTensor(label).unsqueeze(0)
-            data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, smi=smi, y=y)
-            data_list.append(data)
-
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
-
-        data, slices = self.collate(data_list)
-        torch.save((data, slices), self.processed_paths[0])
+            data_dict = self.smiles2datadict(
+                drugbank_total['Drug1_SMILES'].to_list() + drugbank_total['Drug2_SMILES'].to_list())
+            torch.save(data_dict, self.processed_paths[1])
+        else:
+            raise ValueError('Unknown dataset {}!'.format(self.dataset))
 
     @staticmethod
     def is_valid_smiles(smi):
         try:
-            Chem.MolToSmiles(Chem.MolFromSmiles(smi), isomericSmiles=True)
+            Chem.MolToSmiles(Chem.MolFromSmiles(smi))
         except:
             print("not successfully processed smiles: ", smi)
             return False
         return True
 
-    def split(self, type):
-        save_path = Path(self.processed_paths[0]).parent / 'split_{}_{}_{}.ckpt'.format(self.split_seed, self.dataset,
-                                                                                        type)
+    def split(self, type, seed=0):
+        seed_torch(seed)
+        save_path = Path(self.processed_paths[0]).parent / 'split_{}_{}_{}.ckpt'.format(self.dataset, type, seed)
         if save_path.exists():
             trn, val, test = torch.load(save_path)
             return trn, val, test
-        elif type == 'random':
+        elif type == 'random' and self.dataset in ['drugbank_caster']:
             shuffled = self.shuffle()
-            train_size = int(0.8 * len(shuffled))
-            val_size = int(0.1 * len(shuffled))
-            trn = shuffled[:train_size]
-            val = shuffled[train_size:(train_size + val_size)]
-            test = shuffled[(train_size + val_size):]
+            train_size = int(0.7 * len(shuffled))
+            valid_size = int(0.1 * len(shuffled))
+            trn = shuffled[: train_size]
+            val = shuffled[train_size: train_size + valid_size]
+            test = shuffled[train_size + valid_size:]
             torch.save([trn, val, test], save_path)
-            return trn, val, test
-        elif type == 'scaffold':
-            shuffled = self.shuffle()
-            trn, val, test = random_scaffold_split(dataset=shuffled, smiles_list=shuffled.data.smi,
-                                                   null_value=-1, seed=self.split_seed)
-            torch.save([trn, val, test], save_path)
+            assert len(trn) + len(val) + len(test) == len(self)
             return trn, val, test
         else:
             self.log('Error: Unknown split type!!')
 
-    @property
-    def tasks(self):
-        from utils import toxcast_tasks
-        d = {
-            'demo': ['label'],
-            'muv': ["MUV-466", "MUV-548", "MUV-600", "MUV-644", "MUV-652", "MUV-689", "MUV-692", "MUV-712", "MUV-713",
-                    "MUV-733", "MUV-737", "MUV-810", "MUV-832", "MUV-846", "MUV-852", "MUV-858", "MUV-859"],
-            'tox21': ['NR-AR', 'NR-AR-LBD', 'NR-AhR', 'NR-Aromatase', 'NR-ER', 'NR-ER-LBD', 'NR-PPAR-gamma', 'SR-ARE',
-                      'SR-ATAD5', 'SR-HSE', 'SR-MMP', 'SR-p53'],
-            'toxcast': toxcast_tasks,
-            'sider': ['SIDER1', 'SIDER2', 'SIDER3', 'SIDER4', 'SIDER5', 'SIDER6', 'SIDER7', 'SIDER8', 'SIDER9',
-                      'SIDER10', 'SIDER11', 'SIDER12', 'SIDER13', 'SIDER14', 'SIDER15', 'SIDER16', 'SIDER17', 'SIDER18',
-                      'SIDER19', 'SIDER20', 'SIDER21', 'SIDER22', 'SIDER23', 'SIDER24', 'SIDER25', 'SIDER26',
-                      'SIDER27'],
-            'clintox': ['FDA_APPROVED', 'CT_TOX'],
-            'bbbp': ['BBBP'],
-            'bace': ['Class'],
-            'esol': ['measured log solubility in mols per litre'],
-            'freesolv': ['expt'],
-            'lipophilicity': ['exp'],
-            'physprop_perturbed': ['LogP'],
-        }
-        return d[self.dataset]
 
-
-class PertubationDataset(Dataset):
-    def __init__(self, root, dataset='physprop_perturbed', split='scaffold', split_seed=1234, transform=None,
-                 pre_transform=None, pre_filter=None):
-        self.dataset = dataset  # random / random_nan / scaffold
-        self.split_seed = split_seed
-        super(PertubationDataset, self).__init__(root=root, dataset=dataset, split=None, split_seed=None)
-        self.data, self.slices = torch.load(self.processed_paths[0])
-        trn, val, test = self.split(type=split)
-        self.train, self.val, self.test = trn, val, test
-        self.mol_num_node_features = self[0].x.shape[1]
-        self.mol_num_edge_features = self[0].edge_attr.shape[1]
-        self.num_tasks = len(self.tasks)
-
-    def process(self):
-        # load csv
-        df = pd.read_csv(self.raw_paths[0])
-        target = df[self.tasks].values
-        smiles_list = df.SMILES.values
-        data_list = []
-        for i, smi in enumerate(tqdm(smiles_list)):
-            if not self.is_valid_smiles(smi): continue
-            mol = MolFromSmiles(smi)
-            if mol is None: return None
-            x, edge_index, edge_attr = get_mol_nodes_edges(mol)
-            label = target[i]
-            if self.dataset in ['physprop_perturbed']:
-                y = torch.FloatTensor(label).unsqueeze(0)
-            data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, smi=smi, y=y)
-            data_list.append(data)
-
-        if self.pre_filter is not None:
-            data_list = [data for data in data_list if self.pre_filter(data)]
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(data) for data in data_list]
-
-        data, slices = self.collate(data_list)
-        torch.save((data, slices), self.processed_paths[0])
-
-    def split(self, type):
-        save_path = Path(self.processed_paths[0]).parent / 'split_{}_{}_{}.ckpt'.format(self.split_seed, self.dataset,
-                                                                                        type)
-        if save_path.exists():
-            trn, val, test = torch.load(save_path)
-            return trn, val, test
-        df = pd.read_csv(self.raw_paths[0])
-        train_size = len(df[df['label'] == 'train'])
-        val_size = len(df[df['label'] == 'val'])
-        trn, val, test = self[:train_size], self[train_size:train_size + val_size], self[train_size + val_size:]
-        torch.save([trn, val, test], save_path)
-        return trn, val, test
-
-
-def preprocss(smiles_list, labels):
-    data_list = []
-    for smi, label in tqdm(zip(smiles_list, labels)):
-        x, edge_index, edge_attr = get_mol_nodes_edges(Chem.MolFromSmiles(smi))
-        data_list.append(Data(x=x, edge_index=edge_index, edge_attr=edge_attr, smi=smi, y=torch.tensor(label)))
-    return data_list
-
-
-def perturb_test(dataset, level, split=None, split_seed=None):
-    labels = {1: 'Mutation_level_1', 2: 'Mutation_level_2', 3: 'Mutation_level_3'}
-    save_root = Path('../../Dataset/GLAM-GP/')
-
-    print('Loading perturbed dataset...')
-    df = pd.read_csv(save_root / 'raw/{}.csv'.format(dataset))
-    col_name = labels[level]
-    test_total = df[df.label == 'test'][df[col_name].notna()]
-    smiles_list = test_total[col_name].to_list()
-    labels = test_total['LogP'].to_list()
-    perturbed_path = save_root / 'processed/perturbed_{}_{}_{}.ckpt'.format(dataset, split_seed, level)
-
-    print('Processing test dataset level {}...'.format(level))
-    _, _, M = torch.load(str(save_root / 'processed/split_{}_{}_{}.ckpt'.format(split_seed, dataset, split)))
-    M.__indices__ = None  # reference to InMemoryDataset.copy method
-    M.data, M.slices = M.collate(preprocss(test_total['SMILES'].to_list(), labels))
-
-    print('Processing perturbed test dataset level {}...'.format(level))
-    _, _, M_prime = torch.load(str(save_root / 'processed/split_{}_{}_{}.ckpt'.format(split_seed, dataset, split)))
-    M_prime.__indices__ = None  # reference to InMemoryDataset.copy method
-    M_prime.data, M_prime.slices = M_prime.collate(preprocss(smiles_list, labels))
-
-    print('mutations/total:{}/{}'.format(len(test_total), len(df[df.label == 'test'])))
-    print('Perturbation done!')
-    Q = test_total['LogP rdkit']
-    Q_prime = test_total['LogP_level_{}'.format(level)]
-    return M, M_prime, Q.to_numpy(), Q_prime.to_numpy(),
+def extract_batch_data(mol_data, id_batch):
+    smis1, smis2, ys = id_batch.smi1, id_batch.smi2, id_batch.y
+    mol1_batch_list = [mol_data[smi] for smi in smis1]  # smi[0] for unpack smi from [['smi1'], ['smi2']...]
+    mol1_batch = Batch().from_data_list(mol1_batch_list)
+    mol2_batch_list = [mol_data[smi] for smi in smis2]
+    mol2_batch = Batch().from_data_list(mol2_batch_list)
+    return mol1_batch, mol2_batch
 
 
 if __name__ == '__main__':
-
-    datasets = ['bbbp', 'bace', 'sider','toxcast', 'tox21', 'esol', 'freesolv',
-                'lipophilicity', 'physprop_perturbed']
-    for dataset in datasets:
-        results = Dataset(root='../../Dataset/GLAM-GP', dataset=dataset)
+    dataset = Dataset('../../Dataset/GLAM-DDI', dataset='drugbank_caster')
