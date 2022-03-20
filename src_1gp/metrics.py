319dd38a5c1018c8d77b5e5376c4631c38706c33
leo <yvquan.li@gmail.com>
2022-03-20 18:58:00 +0800

    Update metrics.py

--- src_1gp/metrics.py
+++ src_1gp/metrics.py
@@ -1,187 +1,70 @@
-from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, f1_score
-from sklearn import metrics
-from sklearn.metrics import precision_recall_curve
-from dataset import dataset_names
-import numpy as np
 import torch
+from torch_geometric.nn import Set2Set
+from layer import _None
+from layer import GlobalPool5, GlobalLAPool
+from layer import LinearBlock, MessageBlock
 
-def auto_metrics(dataset: str):
-    metrics = ['valauc', 'auc']
-    if dataset in dataset_names["r"] + ['physprop_perturb']:
-        metrics = ['valr2', 'r2']
-    # if dataset.startswith('lit_'): metrics = ['valbedroc', 'bedroc']
-    return metrics
+def model_args(args):
+    _other_args_name = ['dataset_root', 'dataset', 'split', 'seed', 'gpu', 'note', 'batch_size', 'epochs', 'loss',
+                        'optim', 'k', 'lr', 'lr_reduce_rate', 'lr_reduce_patience', 'early_stop_patience',
+                        'verbose_patience', 'split_seed', 'test']
+    model_args_dict = {}
+    for k, v in args.__dict__.items():
+        if k not in _other_args_name:
+            model_args_dict[k] = v
+    return model_args_dict
 
-def binary_metrics(y_true, y_score, y_pred=None, threshod=0.5):
-    auc = roc_auc_score(y_true, y_score)
-    if y_pred is None:
-        y_pred = (y_score >= threshod).astype(int)
-    acc = accuracy_score(y_true, y_pred)
-    prauc = metrics.auc(precision_recall_curve(y_true, y_score)[
-                        1], precision_recall_curve(y_true, y_score)[0])
-    precision = precision_score(y_true, y_pred, average='macro')
-    recall = recall_score(y_true, y_pred, average='macro')
-    f1 = f1_score(y_true, y_pred, average='macro')
-    d = {'auc': auc, 'prauc': prauc, 'acc': acc,
-         'precision': precision, 'recall': recall, 'f1': f1}
-    return d
+def init_weith_with_gain(modules):
+    for m in modules:
+        if isinstance(m, LinearBlock):
+            torch.nn.init.xavier_uniform_(m.linear.weight, gain=4)  # for pasp expr: avoid collapsed output of a predictor without training
 
 
-def binary_metrics_multi_target_nan(y_true, y_score, y_pred=None, nan_fill=-1, threshod=0.5):
-    '''
-       y_true and y_score should be `(N, T)` where N = number of GLAMs, and T = number of targets
-    '''
-    if y_pred is None:
-        y_pred = (y_score >= threshod).astype(int)
-    roc_list, acc_list, prc_list, rec_list = [], [], [], []
-    for i in range(y_true.shape[1]):
-        if (y_true[:, i] == 1).sum() == 0 or (y_true[:, i] == 0).sum() == 0:
-            print('Skipped target, cause AUC is only defined when there is at least one positive data.')
-            continue
+class Architecture(torch.nn.Module):
+    def __init__(self, mol_in_dim=15,
+                 mol_edge_in_dim=4,
+                 hid_dim_alpha=4, e_dim=1024, out_dim=1,
+                 mol_block='_NNConv', message_steps=3,
+                 mol_readout='GlobalPool5',
+                 pre_norm='_None', graph_norm='_None', flat_norm='_None', end_norm='_None',
+                 pre_do='_None()', graph_do='Dropout(0.2)', flat_do='_None()', end_do='Dropout(0.2)',
+                 pre_act='RReLU', graph_act='RReLU', flat_act='RReLU',  # end_act='RReLU',
+                 graph_res=True,
+                 ):
+        super(Architecture, self).__init__()
+        hid_dim = mol_in_dim * hid_dim_alpha
+        self.mol_lin0 = LinearBlock(mol_in_dim, hid_dim, norm=pre_norm, dropout=pre_do, act=pre_act)
+        self.mol_conv = MessageBlock(hid_dim, hid_dim, mol_edge_in_dim,
+                                     norm=graph_norm, dropout=graph_do, conv=mol_block, act=graph_act, res=graph_res)
+        self.message_steps = message_steps
 
-        if nan_fill == -1:
-            is_valid = y_true[:, i] >= 0
-            y_true_st = y_true[is_valid, i]
-            y_score_st = y_score[is_valid, i]
-            y_pred_st = y_pred[is_valid, i]
-            roc_list.append(roc_auc_score(y_true_st, y_score_st))
-            acc_list.append(accuracy_score(y_true_st, y_pred_st))
-            prc_list.append(precision_score(y_true_st, y_pred_st))
-            rec_list.append(recall_score(y_true_st, y_pred_st))
-    d = {'auc': sum(roc_list) / len(roc_list), 'acc': sum(acc_list) / len(acc_list),
-         'precision': sum(prc_list) / len(prc_list), 'recall': sum(rec_list) / len(rec_list)}
-    return d
+        exec('self.mol_readout = {}(in_channels=hid_dim, processing_steps=3)'.format(mol_readout))
+        _mol_ro = 5 if mol_readout == 'GlobalPool5' else 2
+        self.mol_flat = LinearBlock(_mol_ro * hid_dim, e_dim, norm=flat_norm, dropout=flat_do, act=flat_act)
+        self.lin_out1 = LinearBlock(e_dim, out_dim, norm=end_norm, dropout=end_do, act='_None')
+        init_weith_with_gain(self.modules())  # for pasp expr
 
+    def forward(self, data_mol):
+        # pre linear
+        xm = self.mol_lin0(data_mol.x, batch=data_mol.batch)
 
-def cal_ci(y, f):
-    ind = np.argsort(y)
-    y = y[ind]
-    f = f[ind]
-    i = len(y) - 1
-    j = i - 1
-    z = 0.0
-    S = 0.0
-    while i > 0:
-        while j >= 0:
-            if y[i] > y[j]:
-                z = z + 1
-                u = f[i] - f[j]
-                if u > 0:
-                    S = S + 1
-                elif u == 0:
-                    S = S + 0.5
-            j = j - 1
-        i = i - 1
-        j = i - 1
-    ci = S / z
-    return ci
+        # graph conv
+        hm = None
+        for i in range(self.message_steps):
+            xm, hm = self.mol_conv(xm, data_mol.edge_index, data_mol.edge_attr, h=hm, batch=data_mol.batch)
+
+        # readout
+        outm = self.mol_readout(xm, data_mol.batch)
+
+        # final linear
+        outm = self.mol_flat(outm)
+        out = self.lin_out1(outm)
+        return out
 
 
-def regression_metrics(y_true, y_pred):
-    # mae = mean_absolute_error(y_true, y_pred)
-    ci = cal_ci(y_true, y_pred)
-    mse = mean_squared_error(y_true, y_pred)
-    rmse = mse ** 0.5
-    r2 = r2_score(y_true, y_pred)
-    d = {'ci': ci, 'mse': mse, 'rmse': rmse, 'r2': r2}
-    return d
+Model = Architecture
 
-
-def bedroc_score(y_true, y_score, decreasing=True, alpha=20.0):
-    # https://github.com/lewisacidic/scikit-chem/blob/master/skchem/metrics.py
-    big_n = len(y_true)
-    n = sum(y_true == 1)
-    if decreasing:
-        order = np.argsort(-y_score)
-    else:
-        order = np.argsort(y_score)
-
-    m_rank = (y_true[order] == 1).nonzero()[0] + 1
-    s = np.sum(np.exp(-alpha * m_rank / big_n))
-    r_a = n / big_n
-    rand_sum = r_a * (1 - np.exp(-alpha)) / (np.exp(alpha / big_n) - 1)
-    fac = r_a * np.sinh(alpha / 2) / (np.cosh(alpha / 2) -
-                                      np.cosh(alpha / 2 - alpha * r_a))
-    cte = 1 / (1 - np.exp(alpha * (1 - r_a)))
-    return s * fac / rand_sum + cte
-
-
-def enrichment_factor_single(y_true, y_score, threshold=0.005):
-    # https://github.com/gitter-lab/pria_lifechem/blob/1fd892505a/pria_lifechem/evaluation.py
-    labels_arr, scores_arr, percentile = y_true, y_score, threshold
-    non_missing_indices = np.argwhere(labels_arr != -1)[:, 0]
-    labels_arr = labels_arr[non_missing_indices]
-    scores_arr = scores_arr[non_missing_indices]
-    # determine number mols in subset
-    GLAM_size = int(labels_arr.shape[0] * percentile)
-    # get the index positions for these in library
-    indices = np.argsort(scores_arr, axis=0)[::-1][:GLAM_size]
-    # count number of positive labels in library
-    n_actives = np.nansum(labels_arr)
-    # count number of positive labels in subset
-    n_experimental = np.nansum(labels_arr[indices])
-
-    if n_actives > 0.0:
-        ef = float(n_experimental) / n_actives / \
-            percentile  # calc EF at percentile
-    else:
-        raise Exception('n actives == 0')
-    # return n_actives, ef, ef_max
-    return ef
-
-
-def screening_metrics(y_true, y_score, y_pred=None, threshod=0.5):
-    auc = roc_auc_score(y_true, y_score)
-    if y_pred is None:
-        y_pred = (y_score > threshod).astype(int)
-    acc = accuracy_score(y_true, y_pred)
-    precision = precision_score(y_true, y_pred)
-    recall = recall_score(y_true, y_pred)
-    bedroc = bedroc_score(y_true, y_score)
-    ef_001 = enrichment_factor_single(y_true, y_score, 0.001)
-    ef_005 = enrichment_factor_single(y_true, y_score, 0.005)
-    ef_01 = enrichment_factor_single(y_true, y_score, 0.01)
-    ef_02 = enrichment_factor_single(y_true, y_score, 0.02)
-    ef_05 = enrichment_factor_single(y_true, y_score, 0.05)
-    d = {'auc': auc, 'acc': acc, 'precision': precision, 'recall': recall, 'bedroc': bedroc, 'ef_001': ef_001,
-         'ef_005': ef_005, 'ef_01': ef_01, 'ef_02': ef_02, 'ef_05': ef_05, }
-    return d
-
-
-
-def blend_regression(outputs: list, opt='mean', return_pred=False):
-    ls, pls = [], []
-    for _l, _pl in outputs:
-        ls.append(_l)
-        pls.append(_pl)
-    blendd_l = ls[0]
-    blendd_pl = torch.stack(pls, dim=1).mean(dim=1) if opt == 'mean' else None
-    if return_pred is True:
-        return blendd_pl
-    return regression_metrics(blendd_l.numpy(), y_pred=blendd_pl.numpy())
-
-
-def blend_binary_classification(outputs: list, opt='vote', metrics_fn=binary_metrics):
-    ls, pls, ss = [], [], []
-    for _l, _pl, _s in outputs:
-        ls.append(_l)
-        pls.append(_pl)
-        ss.append(_s)
-    blendd_l = ls[0]
-    blendd_pl = torch.stack(pls, dim=1).mode(
-        dim=1)[0] if opt == 'vote' else None
-    blendd_ss = torch.stack(ss, dim=1).mean(dim=1)
-    return metrics_fn(blendd_l.numpy(), y_score=blendd_ss.numpy(), y_pred=blendd_pl.numpy())
-
-
-def blend_binary_classification_mt(outputs: list, opt='vote', metrics_fn=binary_metrics):
-    ls, ss = [], []
-    for _s, _l in outputs:
-        ls.append(_l)
-        ss.append(_s)
-    blendd_l = ls[0]
-    # blendd_pl = torch.stack(pls, dim=2).mode(dim=2)[0] if opt == 'vote' else None
-    blendd_ss = torch.stack(ss, dim=2).mean(dim=2)
-    return metrics_fn(blendd_l.numpy(), y_score=blendd_ss.numpy())
-
+if __name__ == '__main__':
+    mol_batch = torch.load('./other/test_batch.pt')
+    model = Model()
+    result = model(mol_batch)
